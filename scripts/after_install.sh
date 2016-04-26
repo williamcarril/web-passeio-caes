d5882311ede0e73a137f667d10bfe04b1c37fe50
William Carril <williamcarril@terra.com.br>
2016-04-26 17:13:15 -0300

    Corrigindo fluxo de permissões durante deploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,2 +1,6 @@
 cd /var/www/tcc
+mkdir -p -- "vendor"
+chmod 777 vendor
+chmod 777 -R storage
+chmod 777 bootstrap/cache
 composer install
--- scripts/before_install.sh
+++
@@ -1,5 +0,0 @@
-cd /var/www/tcc
-mkdir -p -- "vendor"
-chmod 777 vendor
-chmod 777 -R storage
-chmod 777 bootstrap/cache
